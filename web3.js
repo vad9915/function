0f6ff83bbb8206e1ed578d6641718563c2549b5f
vad9915 <49940065+vad9915@users.noreply.github.com>
2020-02-06 18:31:31 +0300

    Add files via upload

---
+++ web3.js
@@ -0,0 +1,21 @@
+function func(a,b){
+	if (a != b ){
+		console.log("a не дорівнює b")
+	}
+	else 
+	{
+		console.log("a дорівнює b")
+	}
+}
+func(5, 5)
+
+var func2 = function (a,b){
+	if (a ==b ){
+		console.log("a дорівнює b")
+	}
+	else{
+		console.log("a не дорівнює b")
+	}
+}
+
+func2(5, 8)
